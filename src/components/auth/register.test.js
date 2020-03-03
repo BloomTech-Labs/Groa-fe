afeee2db120455574f44f61c3300f6213d597a2b
marielapg <mpgonzales77@gmail.com>
2020-03-03 12:56:29 -0800

    updated the nav test

--- src/components/auth/register.test.js
+++ src/components/auth/register.test.js
@@ -4,10 +4,10 @@ import { getAllByTestId } from "../../utils/test-utils.js";
 
 
 // component to be tested.
-import Navigation from "./navigation.js";
+import Register from "./Register.js";
 
 it("renders Register component", () => { 
-  const { container } = render(<Navigation />); 
-  const component = getAllByTestId(container, "Navigation-component"); 
-  expect(component.length).toBe(1); 
+    const { container } = render(<Register />); 
+    const component = getAllByTestId(container, "Register-component"); 
+    expect(component.length).toBe(1); 
 });
