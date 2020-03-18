feed358698262fad207e4c917d5463b3c92df34b
robomantis19 <freefavors@gmail.com>
2020-03-18 15:28:06 -0400

    added axios request

--- src/store/actions/filterActions.js
+++ src/store/actions/filterActions.js
@@ -1,6 +1,16 @@
-
+import axios from 'axios'; 
 export const SET_FILTER_ITEM = "SET_FILTER_ITEM";
 
-export const setFilter = (item) => { 
-     return {type: SET_FILTER_ITEM, payload: item}
+export const setFilter = (item) => {
+     return dispatch => {
+          axios.get(`https://groaberecommendations-env.eba-jm9gzcte.us-east-1.elasticbeanstalk.com/api/users/movies`, {search: item})
+          .then(res => { 
+               console.log(res)
+               // dispatch({type: SET_FILTER_ITEM, payload: item})
+          })
+          .catch(err => {
+               console.log("Error", err); 
+          })
+     }
+     
 }
