7b9d515ee1718fc33650a91e0a4e06594acfe804
robomantis19 <freefavors@gmail.com>
2020-03-18 16:11:47 -0400

    changed to a post request

--- src/store/actions/filterActions.js
+++ src/store/actions/filterActions.js
@@ -3,7 +3,7 @@ export const SET_FILTER_ITEM = "SET_FILTER_ITEM";
 
 export const setFilter = (item) => {
      return dispatch => {
-          axiosWithAuth().get(`/movies`, {search: item})
+          axiosWithAuth().post(`/movies`, {search: item})
           .then(res => { 
                console.log(res)
                dispatch({type: SET_FILTER_ITEM, payload: res})
