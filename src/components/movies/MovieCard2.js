3665fbf287e6e8e1eb8fc76a8643966e32131f20
robomantis19 <freefavors@gmail.com>
2020-03-23 17:44:45 -0400

    added stars

--- src/components/movies/MovieCard2.js
+++ src/components/movies/MovieCard2.js
@@ -1,7 +1,10 @@
-import React from "react";
-
+import React, {useState} from "react";
+import Stars from "@material-ui/lab/Rating";
 // more fields will be appearing according to the Figma file
 export default function MovieCard2({ name, year, image, genres, runtime }) {
+  const [value, setValue] = useState(0);
+ 
+ 
   return (
     <div data-test="box" className="box">
       <img src={image} alt="Random Movie poster as a placeholder." />
@@ -10,6 +13,14 @@ export default function MovieCard2({ name, year, image, genres, runtime }) {
         <p>{year}</p>
         <p>{genres.split(',').join(" ")}</p>
         <p>{Number(runtime)} min.</p>
+        <Stars data-test="star"
+        precision={0.5}
+          name={name}
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+        />
       </div>
     </div>
   );
